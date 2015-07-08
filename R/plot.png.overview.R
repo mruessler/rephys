e206830afab2fff3399ed249a0337e39522b87a1
Martin Rüßler <martin.ruessler@mailbox.org>
2015-07-08 15:22:12 +0200

    Fixed a bug which would not plot an overview of half of the data.

--- R/plot.png.overview.R
+++ R/plot.png.overview.R
@@ -18,9 +18,21 @@ plot.png.overview <- function(wd = NA) {
 	# plot the data from the files 
 	print("Start writing png files")
 	for (i in 1:length(files)) {
-		tsdata <- ts(data = data[i], start = 1/25000, end = 10, deltat = 1/25000)
-		png(width = 960, filename = sub(pattern = ".csv", replacement = ".png", x = paste(wd, "/png/", files, sep = "")[i]))
-		plot(tsdata, xlab = "Time (s)", ylab = "Amplitude", main = files[i])
-		dev.off()
+		# dual recordings
+		if (ncol(data) == 2 * length(files)) {
+			tsdata <- ts(data = data[((i * 2) - 1):(i * 2)], start = 1/25000, end = 10, deltat = 1/25000)
+			png(width = 960, filename = sub(pattern = ".csv", replacement = ".png", x = paste(wd, "/png/", files, sep = "")[i]))
+			par(mfrow = c(2, 1), bty = "n")
+			plot(tsdata[, 1], xlab = "", ylab = "Amplitude", xaxt = "n", main = files[i])
+			plot(tsdata[, 2], xlab = "Time (s)", ylab = "Amplitude")
+			dev.off()
+		}
+		# single recordings
+		else {
+			tsdata <- ts(data = data[i], start = 1/25000, end = 10, deltat = 1/25000)
+			png(width = 960, filename = sub(pattern = ".csv", replacement = ".png", x = paste(wd, "/png/", files, sep = "")[i]))
+			plot(tsdata, xlab = "Time (s)", ylab = "Amplitude", main = files[i])
+			dev.off()
+		}
 	}
 }
