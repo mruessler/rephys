45ee53a44707accb56320718bf2c43fdfeca5e3e
Martin Rüßler <martin.ruessler@mailbox.org>
2015-07-13 16:17:05 +0200

    removed some parameters from par which do not belong there.

--- R/plot.stimulus.R
+++ R/plot.stimulus.R
@@ -3,9 +3,9 @@
 #' @description add stimuli to a plot. the stimuli data contain information about the position of the stimuli
 #' @param stimdf a data frame containing stimulus data
 plot.stimulus <- function(stimdf) {
-	par(col = "darkgrey", axes = F, xlab = NA, ylab = NA)
-	plot(stimdf$time, stimdf$leftbar, type = "l")
-	plot(stimdf$time, stimdf$rightbar, type = "l")
+	par(col = "darkgrey")
+	plot(stimdf$time, stimdf$leftbar, type = "l", axes = F, xlab = NA, ylab = NA)
+	plot(stimdf$time, stimdf$rightbar, type = "l", axes = F, xlab = NA, ylab = NA)
 	Axis(side = 1, col = "grey")
 	mtext(side = 4, line = 3, "stimulus")
 }
