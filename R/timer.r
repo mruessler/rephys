a61840475e99355e0c7238e267639a4e625358fc
mruessler <mruessler@89614728-fc3b-44d6-b5e6-d6ee55b01582>
2015-07-16 17:50:37 +0000

    Delete everything. Again.
    git-svn-id: https://lamina.biologie.uni-bielefeld.de/project/proj-VisAttIn@23 89614728-fc3b-44d6-b5e6-d6ee55b01582

--- R/timer.r
+++
@@ -1,9 +0,0 @@
-timer <- function(time.start = FALSE) {
-	# a small timer function which returns the current time or the time difference to a time object
-	if (time.start == FALSE) {
-		return(Sys.time())
-	}
-	time.end <- Sys.time()
-	timediff <- difftime(time.end, time.start, units = "secs")
-	return(round(timediff))
-}
