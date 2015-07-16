1ad7c07c748d3d637edc9e3078515fe84ccdd3a5
mruessler <mruessler@89614728-fc3b-44d6-b5e6-d6ee55b01582>
2015-07-16 17:50:33 +0000

    simplified the startanalysis script. Functions can be reloaded with the devtools package and do not require this workaround.
    git-svn-id: https://lamina.biologie.uni-bielefeld.de/project/proj-VisAttIn@17 89614728-fc3b-44d6-b5e6-d6ee55b01582

--- R/startanalysis.r
+++ R/startanalysis.r
@@ -3,19 +3,13 @@ startanalysis <- function(mc = TRUE) {
 	#' @param mc enables or disables the use of multiple cores
 	#' @return ephys raw data to be further processed
 
-	wd <- getwd()
-	# load all the scripts
-	scripts <- paste("R/", dir("R", pattern = "*.r$"), sep = "")
-	if (length(scripts) > 1) {
-		sapply(scripts, source, .GlobalEnv)
-	}
 	# temporarily function for testing purposes
 	# setwd("~/datarepos/ephys/data")
 	# 	setwd("~/data/h1example")
 	#  	setwd("~/data/h1artificial")
-	setwd("~/wolke/work/ephys/data")
-	files <- dir()
+	# select a data dir
+	dd <- "~/wolke/work/ephys/data"
+	files <- dir(dd)
 	data <- readephysdata(files, mc)
-	# 	setwd(wd)
 	return(data)
 }
