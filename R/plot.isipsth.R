e5fd8db55a87c5afb66e8e1c07bca59ef22c338f
Martin Rüßler <martin.ruessler@mailbox.org>
2015-07-27 13:05:40 +0200

    Initial work on the plot.isipsth() function.

---
+++ R/plot.isipsth.R
@@ -0,0 +1,13 @@
+#' plot.isipsth
+#'
+#' @param spikes a spike data frame
+#' @param binwidth
+#' @param samplerate
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot.isipsth <- function(spikes, binwidth, samplerate) {
+
+}
