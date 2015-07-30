96b8eefffb3d27f8a6d0b7f17e26125e6a4e3c81
Martin Rüßler <martin.ruessler@mailbox.org>
2015-07-30 14:56:17 +0200

    Continued work on the plot.data() function. During the process I separated a helper function, vplayout() from within the code. The grid based solution is now fully working.

--- R/plot.data.R
+++ R/plot.data.R
@@ -15,21 +15,25 @@ plot.data <- function(spikes, filelist, stimlist, outputdir) {
 	while (i < length(filelist)) {
 		png(width = 1500, height = 1000, filename = sub(pattern = ".csv", replacement = "--spikes.png", x = paste0(outputdir, filelist)[i]))
 		grid::grid.newpage()
-		grid::pushViewport(grid::viewport(layout = grid::grid.layout(1, 2)))
-		vplayout <- function(x, y) {
-			grid::viewport(layout.pos.row = x, layout.pos.col = y)
-		}
+		grid::pushViewport(grid::viewport(layout = grid::grid.layout(3, 1)))
 		# cut a slice of data for ggplot consumption
+		time <- 1:nrow(spikes)
 		curr.spikes <- data.frame(time, spikes[, i:(i + 1)])
 		# name the columns for easy attribution
 		colnames(curr.spikes) <- c("time", "left", "right")
-		ggspikes <- ggplot2::ggplot(data = spikes[, i], ggplot2::aes(x = 1:nrow(spikes), y = left)) +
-			ggplot2::geom_point(stat = "identity")
- 		print(ggspikes, vp = vplayout(1, 1:2))
-# 		print(b, vp = vplayout(2, 1))
+		# remove unnecessary zero values
+		curr.spikes[curr.spikes == 0] <- NA
+		curr.spikes$right <- curr.spikes$right + 1
+		ggspikes <- ggplot2::ggplot(data = curr.spikes, ggplot2::aes(x = time, y = left)) +
+			ggplot2::geom_point(stat = "identity") +
+			ggplot2::geom_point(stat = "identity", ggplot2::aes(y = right)) +
+			ggplot2::xlim(0, 3)
+
+ 		print(ggspikes, vp = vplayout(1, 1))
+ 		print(ggspikes, vp = vplayout(2, 1))
 # 		print(c, vp = vplayout(2, 2))
 		dev.off()
-
+		i <- i + 2
 	}
 }
 # ggplot(data = head(topcountries, 10), aes(x = reorder(Country, Earnings), y = Earnings / 1000000)) +
@@ -40,20 +44,3 @@ plot.data <- function(spikes, filelist, stimlist, outputdir) {
 # 	ggtitle("Top earnings per country (2014)") +
 # 	xlab("Country") +
 # 	ylab(expression("Earnings [" ~ 10 ^ {6} ~ "US$]"))
-
-# plot.data <- function(spikes, stimlist, files, pd) {
-# 	# plot the spikes and the stimuli
-# 	start.time <- timer()
-# 	for (i in 1:length(files)) {
-# 		png(width = 1500, filename = sub(pattern = ".csv", replacement = "--spikes.png", x = paste0(pd, files)[i]))
-# 		par(mfrow = c(4, 1), bty = "n", cex = 1)
-# 		tsspikes <- ts(data = spikes[((i * 2) - 1):(i * 2)], start = 1/25000, end = 10, deltat = 1/25000)
-# 		print(head(tsspikes))
-# 		plot.spikes(tsspikes[, 1], xlab = "", ylab = "Left channel", xaxt = "n", main = files[i])
-# 		plot.spikes(tsspikes[, 2], xlab = "", ylab = "Right channel")
-# 		plot.stimulus(stimlist[[i]])
-# 		dev.off()
-# 	}
-# 	time.diff <- timer(start.time)
-# 	writeLines(paste0("Plotting completed (", time.diff, " seconds)."))
-# }
