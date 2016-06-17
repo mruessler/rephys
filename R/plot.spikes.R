614a51b10572cc6307fa242f5fa5f38a6620e708
Martin Rüßler <martin.ruessler@mailbox.org>
2016-06-17 09:35:20 +0200

    Corrected an error in the plot.spikes() function. The variable i went out of bounds in the for loop. I also added a custom error message for future use.

--- R/plot.spikes.R
+++ R/plot.spikes.R
@@ -11,11 +11,14 @@
 #'
 plot.spikes <- function(spikes, filelist, stimlist, outputdir) {
 	# plot the spikes and the stimuli
-	for (i in 1:length(filelist)) {
+	for (i in 1:(length(filelist) / 2)) {
 		par(oma = c(rep(0, 4)), mar = c(1, 1, 1, 1), ps = 20, cex = 10)
 		png(width = 1000, height = 750, filename = sub(pattern = ".csv", replacement = "--spikes.png", x = paste0(outputdir, filelist)[i]))
 		layout(matrix(1:4, 4, 1), heights = c(rep(1/4, 4)))
-		plot.trial(trial = spikes[, ((i * 2) - 1):(i * 2)], filename = filelist[i], stimuli = stimlist[[i]])
+		result <- try(plot.trial(trial = spikes[, ((i * 2) - 1):(i * 2)], filename = filelist[i], stimuli = stimlist[[i]]), silent = TRUE)
+		if (inherits(result, "try-error") == TRUE) {
+			stop(paste("Spike plotting function experienced an error:", geterrmessage()))
+		}
 		dev.off()
 	}
 }
