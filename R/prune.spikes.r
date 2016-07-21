ab4cc3b241599300567ce48777d52bc0a96f80a9
Martin Rüßler <martin.ruessler@mailbox.org>
2016-07-21 15:25:20 +0200

    Merge remote branch 'master' into master.

--- R/prune.spikes.r
+++ R/prune.spikes.r
@@ -1,5 +1,29 @@
 #' prune.spikes
 #'
+#' This file is part of the rephys package.
+#'
+#' Copyright(c) Martin Rüßler
+#'
+#' This file may be licensed under the terms of of the
+#' GNU General Public License Version 3 (the ``GPL''),
+#' or (at your option) any later version.
+#'
+#' Software distributed under the License is distributed
+#' on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+#' express or implied. See the GPL for the specific language
+#' governing rights and limitations.
+#'
+#' You should have received a copy of the GPL along with this
+#' program. If not, go to http://www.gnu.org/licenses/gpl.html
+#' or write to the Free Software Foundation, Inc.,
+#' 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#'
+#' The development of this software was supported by the
+#' Excellence Cluster EXC 277 Cognitive Interaction Technology.
+#' The Excellence Cluster EXC 277 is a grant of the Deutsche
+#' Forschungsgemeinschaft (DFG) in the context of the German
+#' Excellence Initiative.
+#'
 #' @description a copy of the H1toolbox prune_spikes function. This Function removes spikes that fall within the minimum inter stimulus interval of the previous spike. It is safe to assume that a spike within 3 ms can be filtered out.
 #' @param spikes spike matrix to be pruned
 #' @param min.isi minimum inter stimulus interval in milliseconds
@@ -10,7 +34,7 @@
 prune.spikes <- function(spikes, min.isi) {
 	# copy spike matrix
 	prunedspikes <- spikes
-	
+
 	# initialise index of last spike: infinitely before the first one.
  	for (i in 1:ncol(spikes)) {
  		last <- -Inf
@@ -24,7 +48,7 @@ prune.spikes <- function(spikes, min.isi) {
  				}
  			}
  		}
-		
+
 	}
 	return(prunedspikes)
 }
